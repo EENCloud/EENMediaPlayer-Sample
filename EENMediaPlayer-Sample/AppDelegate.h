34d4d5b90cdbb48701c91813ec45633abf63e3db
Greg Slomin <g@client.openvpn.net>
2017-08-07 12:02:31 -0500

    Initial Commit

---
+++ EENMediaPlayer-Sample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EENMediaPlayer-Sample
+//
+//  Created by Greg Slomin on 8/7/17.
+//  Copyright © 2017 Greg Slomin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EENMediaPlayer-Sample/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  EENMediaPlayer-Sample
+//
+//  Created by Greg Slomin on 8/7/17.
+//  Copyright © 2017 Greg Slomin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
